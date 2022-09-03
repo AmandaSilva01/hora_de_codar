2aab0ac0807be92c78acd496cc2cc5a9af71451a
AmandaSilva01 <110691774+AmandaSilva01@users.noreply.github.com>
2022-09-03 18:28:10 -0300

    atividade hora de codar 1

---
+++ hora de codar.js
@@ -0,0 +1,48 @@
+/*resposta 1
+var nome_do_carro = fusca 
+*/
+/*resposta 2
+var nome_do_usuario = prompt("diga seu nome");
+alert ("olá, "+ nome_do_usuario);
+*/
+/*resposta 3
+var nome_do_usuario = prompt("diga seu nome");
+var idade_do_usuario = prompt ("diga sua idade");
+alert ("olá, " + nome_do_usuario + " sua idade é: "+ idade_do_usuario);
+*/
+/* resposta 4 - retângulo
+var base = parseInt(prompt("insira o valor da base"));
+var altura = parseInt (prompt("insira o valor da altura"));
+alert ("o resultado é: " + base*altura);
+*/
+/* - quadrado
+var base = parseInt(prompt("insira o valor do lado 1"));
+var altura = parseInt (prompt("insira o valor do lado 2"));
+alert ("o resultado é: " + base*altura);
+*/
+/* - losango
+var Dmaior = parseInt(prompt("insira o valor da diagonal maior"));
+var Dmenor = parseInt (prompt("insira o valor da diagonal menor"));
+alert ("o resultado é: " + Dmaior*Dmenor/2);
+*/
+/* - trapézio
+var Bmaior = parseInt(prompt("insira o valor da diagonal maior"));
+var Bmenor = parseInt (prompt("insira o valor da diagonal menor"));
+var h = parseInt(prompt("insira o valor da altura"))
+alert ("o resultado é: " + (Bmaior*Bmenor)*h/2);
+*/
+/* - paralelogramo
+var base = parseInt(prompt("insira o valor da base"));
+var altura = parseInt (prompt("insira o valor da altura"));
+alert ("o resultado é: " + base*altura);
+*/
+/* - triângulo
+var base = parseInt(prompt("insira o valor da base"));
+var altura = parseInt (prompt("insira o valor da altura"));
+alert ("o resultado é: " + base*altura/2);
+*/
+/* - círculo
+var base = parseInt(prompt("insira o valor de Pi (π)"));
+var altura = parseInt (prompt("insira o valor do R² "));
+alert ("o resultado é: " + base*altura);
+*/
